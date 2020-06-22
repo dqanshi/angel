b4f7ea4c27b306e783bec7dfa245cc197b282c17
dqsalmaan <63580147+dqsalmaan@users.noreply.github.com>
2020-06-22 21:16:06 +0530

    Update police.py

--- alluka/modules/police.py
+++ alluka/modules/police.py
@@ -6,10 +6,10 @@ from typing import List
 from telegram import Bot, Update, ParseMode
 from telegram.ext import run_async
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin
-from tg_bot.modules.helper_funcs.extraction import extract_user
+from alluka import dispatcher
+from alluka.modules.disable import DisableAbleCommandHandler
+from alluka.modules.helper_funcs.chat_status import is_user_admin, user_admin
+from alluka.modules.helper_funcs.extraction import extract_user
 
 #sleep how many times after each edit in 'police' 
 EDIT_SLEEP = 1
@@ -30,7 +30,7 @@ def police(bot: Bot, update: Update):
     for x in range(EDIT_TIMES):
         msg.edit_text(police_siren[x%2])
         time.sleep(EDIT_SLEEP)
-    msg.edit_text('Police is here!')
+    msg.edit_text('DQ Police is here!')
 
 
 __help__ = """
